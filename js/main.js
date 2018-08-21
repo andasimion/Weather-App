766851d96932be4f5c011838ef0b8bf2b7cee79e
andasimion <anda.simion90@gmail.com>
2018-08-21 15:51:21 +0300

    Changed declaration of variables from 'var' to 'let', changed the createdForecastEveryThreeHours function to return a list and added dates to the dropdown forecast button

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function getShortDateFromUnixTime(unixSeconds) {
-    var d = new Date(0);
+    let d = new Date(0);
     d.setUTCSeconds(unixSeconds);
     options = { 
         weekday: 'short',
@@ -10,26 +10,27 @@ function getShortDateFromUnixTime(unixSeconds) {
 }
 
 function getTimeFromUnixTime(unixSeconds) {
-    var d = new Date(0);
+    let d = new Date(0);
     d.setUTCSeconds(unixSeconds);
-    return d.toLocaleTimeString(undefined, {
+    options = {
         hour: '2-digit',
         minute: '2-digit',
         second: '2-digit'
-    })
+    };
+    return d.toLocaleTimeString(undefined, options)
 }
 
 
-var weatherConditions = new XMLHttpRequest();
+let weatherConditions = new XMLHttpRequest();
 weatherConditions.open('GET', 'weather_info.txt', true);
 weatherConditions.responseType = 'text';
 weatherConditions.send();
 
 weatherConditions.onload = function() {
     if (weatherConditions.status === 200) {
-        var weatherObj = JSON.parse(weatherConditions.responseText);
+        let weatherObj = JSON.parse(weatherConditions.responseText);
         console.log(weatherObj);
-        var localTime = new Date(0);
+        let localTime = new Date(0);
         localTime.setUTCSeconds(weatherObj.dt);
         document.getElementById('dt').innerHTML =  `${getTimeFromUnixTime(weatherObj.dt)}, ${getShortDateFromUnixTime(weatherObj.dt)}`;
         document.getElementById('weatherIcon').setAttribute("src", `http://openweathermap.org/img/w/${weatherObj.weather["0"].icon}.png`);
@@ -43,38 +44,36 @@ weatherConditions.onload = function() {
     }
 }
 
-// creates a object with the meteorological readings from the forecastObj
+// creates a list with 40 meteorological readings from the forecastObj
 function createForecastEveryThreeHourObj(forecastObj) {
     
-    var forecastEveryThreeHours = new Object;
+    let forecastEveryThreeHours = [];
 
     for (var i = 0; i < forecastObj.list.length; i++) {
-        forecastEveryThreeHours[i] = new Object;
-        forecastEveryThreeHours[i]['dt'] = forecastObj.list[i].dt;
-        forecastEveryThreeHours[i]['temp'] = `${forecastObj.list[i].main.temp}&#8451`;
-        forecastEveryThreeHours[i]['description'] = `http://openweathermap.org/img/w/${forecastObj.list[i].weather["0"].icon}.png`;
-        forecastEveryThreeHours[i]['wind'] = `${forecastObj.list[i].wind.speed}m/s`;
-        forecastEveryThreeHours[i]['pressure'] = `${forecastObj.list[i].main.pressure}hPa`;
-        forecastEveryThreeHours[i]['humidity'] = `${forecastObj.list[i].main.humidity}%`;  
+        forecastEveryThreeHours.push(new Object);
+        forecastEveryThreeHours[i].dt = forecastObj.list[i].dt;
+        forecastEveryThreeHours[i].temp = `${forecastObj.list[i].main.temp}&#8451`;
+        forecastEveryThreeHours[i].description = `http://openweathermap.org/img/w/${forecastObj.list[i].weather["0"].icon}.png`;
+        forecastEveryThreeHours[i].wind = `${forecastObj.list[i].wind.speed}m/s`;
+        forecastEveryThreeHours[i].pressure = `${forecastObj.list[i].main.pressure}hPa`;
+        forecastEveryThreeHours[i].humidity = `${forecastObj.list[i].main.humidity}%`; 
     };
-
-    forecastEveryThreeHours['length'] = forecastObj.list.length;
     return forecastEveryThreeHours;
 };
 
-// creates the forecast table from the forecastEveryThreeHoursDay object
+// creates the forecast table from the forecastEveryThreeHoursDay list
 function createForecastTable(forecastEveryThreeHoursDay) {
     forecastTable = Array.from(document.getElementById('forecastTableBody').children);
     console.log(forecastTable);
-    for (var i = 0; i < forecastTable.length; i++) {
+    for (let i = 0; i < forecastTable.length; i++) {
         forecastTable[i].innerHTML = "";
-        for (var key in forecastEveryThreeHoursDay[i]) {
-            var tdTag = document.createElement('td');
+        for (let key in forecastEveryThreeHoursDay[i]) {
+            let tdTag = document.createElement('td');
             if (key === 'dt') {
                 tdTag.innerHTML = getTimeFromUnixTime(forecastEveryThreeHoursDay[i][key]);
                 forecastTable[i].appendChild(tdTag);
             } else if (key === 'description') {
-                var imgTag = document.createElement('img');
+                let imgTag = document.createElement('img');
                 imgTag.setAttribute("src", forecastEveryThreeHoursDay[i][key]);
                 imgTag.setAttribute("alt", "icon");
                 imgTag.setAttribute("title", key);
@@ -88,23 +87,29 @@ function createForecastTable(forecastEveryThreeHoursDay) {
     } 
 }
 
+
 function getForecast (index1, index2) {
-    
-    var forecastWeather = new XMLHttpRequest();
+    let forecastWeather = new XMLHttpRequest();
     forecastWeather.open('GET', 'forecast_info.txt', true);
     forecastWeather.responseType = 'text';
     forecastWeather.send();
 
     forecastWeather.onload = function() {
         if (forecastWeather.status === 200) {
-            var forecastObj = JSON.parse(forecastWeather.responseText);
+            let forecastObj = JSON.parse(forecastWeather.responseText);
             console.log(forecastObj);
 
             forecastEveryThreeHours = createForecastEveryThreeHourObj(forecastObj);
-            forecastEveryThreeHoursDay = Array.prototype.slice.call(forecastEveryThreeHours, index1, index2)
+            console.log(forecastEveryThreeHours);
+            console.log(forecastEveryThreeHours[8].dt);
+            console.log(getShortDateFromUnixTime(forecastEveryThreeHours[8].dt));
+            document.getElementById('tomorrow').innerHTML = getShortDateFromUnixTime(forecastEveryThreeHours[8].dt);
+            document.getElementById('theDayAfterTomorrow').innerHTML = getShortDateFromUnixTime(forecastEveryThreeHours[16].dt);
+            document.getElementById('theDayAfterThat').innerHTML = getShortDateFromUnixTime(forecastEveryThreeHours[24].dt);
+            
+            forecastEveryThreeHoursDay = forecastEveryThreeHours.slice(index1, index2);
             console.log(forecastEveryThreeHoursDay);
 
-            
             document.getElementById('forecastDay').innerHTML = getShortDateFromUnixTime(forecastEveryThreeHoursDay[0]['dt']);
             
             createForecastTable(forecastEveryThreeHoursDay);
@@ -112,8 +117,8 @@ function getForecast (index1, index2) {
     }
 }
 
-getForecast(0, 8);
 
+getForecast(0, 8);
 
 document.getElementById('tomorrow').addEventListener('click', function() {getForecast(8,16)});
 document.getElementById('theDayAfterTomorrow').addEventListener('click', function() {getForecast(16, 24)});
